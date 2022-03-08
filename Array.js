1aceb14e9a93bdc73eed8e3d05228aaf1810bc63
ashish-bhadauriya <ashishbhadauriya5344@gmail.com>
2022-03-08 22:00:15 +0530

    Object

---
+++ Array.js
@@ -0,0 +1,10 @@
+let arr=[]
+console.log(arr);
+
+let ele=[1,2,3,"I am String",'c',9.9]
+console.log(ele);
+
+console.log(ele[3]);
+
+ele.push("new item");
+console.log(ele);
---
+++ Object.js
@@ -0,0 +1,12 @@
+//Empty Object
+let obj={};
+console.log(obj);
+
+
+let ObjName={
+    FirstName:"Ashish",
+    LastName:"Singh",
+    Age:20,
+    PhoneNum:9670984783
+}
+console.log(ObjName);
