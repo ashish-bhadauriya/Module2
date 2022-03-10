849f49bad38ef6c9d0662cae4c1b1a34f3c38869
ashish-bhadauriya <ashishbhadauriya5344@gmail.com>
2022-03-10 23:35:47 +0530

    CURD

--- Node/fs.js
+++ Node/fs.js
@@ -5,5 +5,13 @@ let path=require("path");
 
 let filePath=path.join(__dirname,"file.txt");
 console.log(filePath);
-
+console.log("Before update");
 fs.writeFileSync(filePath,"Again writing to execting file !");
+
+
+fs.appendFileSync(filePath,"Newly added content ");
+console.log("afterUpdate");
+
+console.log(fs.readFileSync(filePath,'utf-8'));
+
+//fs.unlinkSync(filePath);//delete file
