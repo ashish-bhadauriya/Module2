5368160fa0a3ddf8e4a20108b3343e7e73c97b6e
ashish-bhadauriya <ashishbhadauriya5344@gmail.com>
2022-03-10 21:49:30 +0530

    childprocess

--- Node/childprocess.js
+++ Node/childprocess.js
@@ -1,5 +1,9 @@
 let cp=require('child_process')
 //console.log(cp);
-cp.execFileSync("calc");
+//cp.execFileSync("calc");
 
-//Ashish bhadauriya
+//cp.execSync("code");
+// let content=cp.execSync("node test .js");
+// console.log("output of test file :"+content);
+let content=cp.execSync("node test . js");
+console.log(""+content);
